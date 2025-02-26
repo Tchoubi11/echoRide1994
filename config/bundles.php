7b0e3b4de10ec859bc935d1fa9c8c7c6cf72d149
Tchoubi11 <tchoubayefranck@gmail.com>
2025-02-26 16:47:10 +0100

    Installation des dépendances et mise à jour et creation des fichiers .env,.gitigonre,composer.lock,importmap.php,symfony.lock,app.js,bootstrap.js,controller.json,app.css,bundle.php,service.yaml,asset_mapper.yaml,doctrine_mongodb.yaml,doctrine.y aml.php,AppFixtures.php et base.html.twig

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,9 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
