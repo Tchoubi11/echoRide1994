1b963e641f0234af06bc92c222a76d4a3b463cc7
Tchoubi11 <tchoubayefranck@gmail.com>
2025-02-28 22:03:16 +0100

    mise a jour covoiturageType.php,home/index.html.twig et search/index.html.twig

--- src/Form/CovoiturageSearchType.php
+++ src/Form/CovoiturageSearchType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class CovoiturageSearchType extends AbstractType
 {
@@ -15,16 +16,37 @@ class CovoiturageSearchType extends AbstractType
         $builder
             ->add('departure', TextType::class, [
                 'label' => 'Départ',
-                'attr' => ['placeholder' => 'Votre départ']
+                'attr' => ['placeholder' => 'Votre départ'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Veuillez saisir un lieu de départ.']),
+                    new Assert\Length([
+                        'max' => 255, 
+                        'maxMessage' => 'Le lieu de départ ne doit pas dépasser {{ limit }} caractères.'
+                    ]),
+                ]
             ])
             ->add('destination', TextType::class, [
                 'label' => 'Destination',
-                'attr' => ['placeholder' => 'Votre destination']
+                'attr' => ['placeholder' => 'Votre destination'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Veuillez saisir une destination.']),
+                    new Assert\Length([
+                        'max' => 255, 
+                        'maxMessage' => 'La destination ne doit pas dépasser {{ limit }} caractères.'
+                    ]),
+                ]
             ])
             ->add('date', DateType::class, [
                 'label' => 'Date',
                 'widget' => 'single_text',
-                'attr' => ['placeholder' => 'Date du trajet']
+                'attr' => ['placeholder' => 'Date du trajet'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Veuillez saisir une date.']),
+                    new Assert\Type([
+                        'type' => \DateTime::class, 
+                        'message' => 'La date doit être valide.'
+                    ]),
+                ]
             ]);
     }
 
